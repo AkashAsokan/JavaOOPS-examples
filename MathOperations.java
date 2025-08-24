d16b7f734a8e033fb575c932db934218befe8d68
Akash A <akashasokan112003@gmail.com>
2025-08-24 22:21:24 +0530

    Add files via upload

---
+++ MathOperations.java
@@ -0,0 +1,27 @@
+public class MathOperations {
+
+    
+    public static int multiplyNumbers(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static void main(String[] args) {
+        
+        int result = MathOperations.multiplyNumbers(10, 5);
+        System.out.println("Result: " + result);
+    }
+}
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
